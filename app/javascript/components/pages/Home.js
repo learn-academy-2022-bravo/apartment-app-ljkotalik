52399183602c5055c771a4b75eff365287055913
ljkotalik <leannak14@gmail.com>
2022-05-17 15:19:07 -0700

    initial commit and added title to landing page

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react'
 export default class Home extends Component {
   render() {
     return (
-      <h3>Home</h3>
+      <>
+      <h3>Welcome to Serenity Falls</h3>
+      <p>Luxury Apartments with a holistic-focused community</p>
+      </>
     )
   }
 }
