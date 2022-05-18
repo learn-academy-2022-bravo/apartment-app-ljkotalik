d2951af3b184853dc863e69a1926acb14f8ba1f4
ljkotalik <leannak14@gmail.com>
2022-05-18 11:13:27 -0700

    added apts list

--- app/javascript/components/pages/ApartmentIndex.js
+++ app/javascript/components/pages/ApartmentIndex.js
@@ -3,7 +3,19 @@ import React, { Component } from 'react'
 export default class ApartmentIndex extends Component {
   render() {
     return (
-      <h3>Apartment Index</h3>
+      <>
+      <h3 className="index">Apartments Currently Available</h3>
+      <ul>
+          <li>Emerald Falls</li>
+          <li>Cascade</li>
+          <li>Tranquility</li>
+          <li>Wanderlust</li>
+          <li>Serendipity</li>
+          <li>Iridescent</li>
+          <li>Mellifluous</li>
+        </ul>
+      </>
     )
   }
 }
+  
